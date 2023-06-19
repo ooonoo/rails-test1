fba420b5672b67aec4df60dde3cd09028469b17f
ooonoo <bscnext63@gmail.com>
2023-06-19 22:06:42 +0900

    投稿fフォームから送信されたデータを受け取り保存する処理を再追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,8 @@ class PostsController < ApplicationController
   def new
   end
   def create
+    @post = Post.new(content:params[:content])
+    @post.save
     redirect_to ("/posts/index")
   end
 end
