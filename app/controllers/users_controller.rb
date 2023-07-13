0b9e0c57bc656cb0f78d47334bf9456063e30fbc
ooonoo <bscnext63@gmail.com>
2023-07-13 20:28:51 +0900

    add: createアクションを仮作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(name:params[:name],email:params[:email])
-    @user.save
-    redirect_to("/users/#{@user.id}")
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
   end
 end
