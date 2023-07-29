8b4a30440c8a6a4894cc238123da51edc2affe25
ooonoo <bscnext63@gmail.com>
2023-07-29 15:43:00 +0900

    ログイン時に合っているか否かで移動先が変わる処理を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,15 @@ class UsersController < ApplicationController
   end
 
   def login
-    
+    @user = User.find_by(email: params[:email],
+                         password: params[:password])
+    # 入力内容と一致するユーザーを取得し変数@userに代入
+    if @user
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      render("users/login_form")
+    end
   end
 
 end
