ba0beec3dd3aba9c073c574d2f26ba2870708da8
ooonoo <bscnext63@gmail.com>
2023-07-30 16:31:09 +0900

    新規登録時にそのままログインされ保持される処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
     )
     
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
