0f49a69279b8fd91bd88d215a7199ea495132ee4
ooonoo <bscnext63@gmail.com>
2023-08-08 10:27:11 +0900

    rails db:migrate postsにuser_idを追加

--- db/migrate/20230808005529_add_user_id_to_posts.rb
+++ db/migrate/20230808005529_add_user_id_to_posts.rb
@@ -1,4 +1,5 @@
 class AddUserIdToPosts < ActiveRecord::Migration[7.0]
   def change
+    add_column :posts, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_29_042750) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_005529) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
